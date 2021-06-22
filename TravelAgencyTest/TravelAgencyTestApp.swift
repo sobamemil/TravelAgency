6eb4b64d8106975d3ec10066065f15fa5fc01aca
sobamemil <sieh96@naver.com>
2021-06-22 14:32:51 +0900

    Initial Commit

---
+++ TravelAgencyTest/TravelAgencyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAgencyTestApp.swift
+//  TravelAgencyTest
+//
+//  Created by 심찬영 on 2021/06/22.
+//
+
+import SwiftUI
+
+@main
+struct TravelAgencyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
