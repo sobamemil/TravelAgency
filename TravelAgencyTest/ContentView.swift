b5874743a975a1a954f1bdc2539df73886fdf130
sobamemil <sieh96@naver.com>
2021-06-23 16:05:51 +0900

    NavigationView 안에 ScrollView 추가

--- TravelAgencyTest/ContentView.swift
+++ TravelAgencyTest/ContentView.swift
@@ -14,36 +14,38 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
-                HStack {
-                    NavigationLink(destination: ListView()) {
-                        Image(systemName: "line.3.horizontal").foregroundColor(.gray).frame(alignment: .leading).font(.title)
-                    } // NavigationLink
-                    Spacer()
+            ScrollView {
+                VStack {
+                    HStack {
+                        NavigationLink(destination: ListView()) {
+                            Image(systemName: "line.3.horizontal").foregroundColor(.gray).frame(alignment: .leading).font(.title)
+                        } // NavigationLink
+                        Spacer()
 
-                    Text("코리아여행사").font(.title)
-                    Spacer()
+                        Text("코리아여행사").font(.title)
+                        Spacer()
 
-                    NavigationLink(destination: Text("user 화면")) {
-                        Image(systemName: "person").foregroundColor(.gray).font(.title)
-                    } // NavigationLink
-                } // HStack
-                .padding(.horizontal, 20)
-                .padding(.top, 10)
-                // .background(Color.yellow)
-                .navigationBarTitle("메인")
-                .navigationBarHidden(self.isNavigationBarHidden)
-                .onAppear {
-                    self.isNavigationBarHidden = true
-                }
-                
-                Image("image_1").resizable().frame(height: 250)
-                    .padding(.bottom, 20)
-                SelectableMenuView()
-                MyDivider()
-                
-                Spacer()
-            } // VStack
+                        NavigationLink(destination: Text("user 화면")) {
+                            Image(systemName: "person").foregroundColor(.gray).font(.title)
+                        } // NavigationLink
+                    } // HStack
+                    .padding(.horizontal, 20)
+                    .padding(.top, 10)
+                    // .background(Color.yellow)
+                    .navigationBarTitle("메인")
+                    .navigationBarHidden(self.isNavigationBarHidden)
+                    .onAppear {
+                        self.isNavigationBarHidden = true
+                    }
+                    
+                    Image("image_1").resizable().frame(height: 250)
+                        .padding(.bottom, 20)
+                    SelectableMenuView()
+                    MyDivider()
+                    
+                    Spacer()
+                } // VStack
+            } // ScrollView
         } // NavigationView
     }
 }
