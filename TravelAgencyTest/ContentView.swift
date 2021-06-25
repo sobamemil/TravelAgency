88b77c5dfa2fa7f24ad30034af01ce0d4bfb37e9
sobamemil <sieh96@naver.com>
2021-06-25 15:41:15 +0900

    sidemenu에 gesture 추가 (ZStack에 drag gesture 추가하여 sidemenu 닫기 기능 구현)

--- TravelAgencyTest/ContentView.swift
+++ TravelAgencyTest/ContentView.swift
@@ -12,39 +12,73 @@ struct ContentView: View {
     @State
     var isNavigationBarHidden: Bool = false
     
+    @State
+    var showMenu = false
+    
     var body: some View {
+        let drag = DragGesture()
+            .onEnded { gesture in
+                if (gesture.translation.width < -100) {
+                    withAnimation {
+                        self.showMenu = false
+                    }
+                }
+            }
+
         NavigationView {
             ScrollView {
-                VStack {
-                    HStack {
-                        NavigationLink(destination: ListView()) {
-                            Image(systemName: "line.3.horizontal").foregroundColor(.gray).frame(alignment: .leading).font(.title)
-                        } // NavigationLink
-                        Spacer()
+                return GeometryReader { geometry in
+                    ZStack {
+                        VStack {
+                            HStack {
+//                                NavigationLink(destination: ListView()) {
+//                                    Image(systemName: "line.3.horizontal").foregroundColor(.gray).frame(alignment: .leading).font(.title)
+//                                } // NavigationLink
+                                
+                                Button(action: {
+                                    withAnimation{ self.showMenu = true }  }) {
+                                    Image(systemName: "line.3.horizontal").foregroundColor(.gray).frame(alignment: .leading).font(.title)
+                                }
+                                Spacer()
 
-                        Text("코리아여행사").font(.title)
-                        Spacer()
+                                Text("코리아여행사").font(.title)
+                                Spacer()
 
-                        NavigationLink(destination: Text("user 화면")) {
-                            Image(systemName: "person").foregroundColor(.gray).font(.title)
-                        } // NavigationLink
-                    } // HStack
-                    .padding(.horizontal, 20)
-                    .padding(.top, 10)
-                    // .background(Color.yellow)
-                    .navigationBarTitle("메인")
-                    .navigationBarHidden(self.isNavigationBarHidden)
-                    .onAppear {
-                        self.isNavigationBarHidden = true
-                    }
+                                NavigationLink(destination: Text("user 화면")) {
+                                    Image(systemName: "person").foregroundColor(.gray).font(.title)
+                                } // NavigationLink
+                            } // HStack
+                            .padding(.horizontal, 20)
+                            .padding(.top, 10)
+                            // .background(Color.yellow)
+                            .navigationBarTitle("메인")
+                            .navigationBarHidden(self.isNavigationBarHidden)
+                            .onAppear {
+                                self.isNavigationBarHidden = true
+                            }
+                            
+                            Image("image_1").resizable().frame(width: geometry.size.width, height: geometry.size.height * 25)
+                                .padding(.bottom, 20)
+                            SelectableMenuView()
+                            MyDivider()
+                            
+                            Spacer()
+                        } // VStack
+                        .frame(width: geometry.size.width)
+                        .offset(x: self.showMenu ? geometry.size.width/2 : 0)
+                            .disabled(self.showMenu ? true : false)
+                        
+                        if self.showMenu {
+                            SideMenuView()
+                                .frame(width: geometry.size.width/2)
+                                .transition(.move(edge: .leading))
+                        }
+                        
+                    } // ZStack
+                    .gesture(drag)
+                } // GeometryReader
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
                     
-                    Image("image_1").resizable().frame(height: 250)
-                        .padding(.bottom, 20)
-                    SelectableMenuView()
-                    MyDivider()
-                    
-                    Spacer()
-                } // VStack
             } // ScrollView
         } // NavigationView
     }
