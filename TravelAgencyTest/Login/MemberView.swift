cbda3d55b7256db322576bb7451f5f70d11d4290
sobamemil <sieh96@naver.com>
2021-07-02 19:06:48 +0900

    MemberView에 로그인 버튼 추가. cornerRadius 사용하여 둥글게 표현, backgroundColor는 themeColor, textColor는 white, text에 shadow 표현 삽입

--- TravelAgencyTest/Login/MemberView.swift
+++ TravelAgencyTest/Login/MemberView.swift
@@ -49,6 +49,17 @@ struct MemberView: View {
             } // HStack
             .frame(maxWidth: (UIScreen.main.bounds.maxX * 0.8), alignment: .trailing )
             .padding()
+            .font(.system(size: 15))
+            
+            Button(action: {}) {
+                Text("로그인")
+                    .font(.system(size: 15))
+                    .foregroundColor(.white)
+                    .shadow(color: .pink, radius: 4, x: 5, y: 5)
+            } // Login Button
+            .frame(width: (UIScreen.main.bounds.maxX * 0.8), height: (UIScreen.main.bounds.maxY * 0.1) / 5).padding(.vertical)
+            .background(themeColor)
+            .cornerRadius(15)
 
             HStack {
                 Button(action: {}) {
@@ -68,6 +79,8 @@ struct MemberView: View {
                 }
             } // HStack
             .font(.system(size: 13))
+            .accentColor(.black)
+            .padding(.top)
         } // VStack
     }
 }
