7f66f92079e3610cdf7efcd39b6b86f5bbb6eab9
sobamemil <sieh96@naver.com>
2021-07-03 19:18:56 +0900

    GuestView의 예약자 성명, 예약 번호, 연락처 필드 추가

--- TravelAgencyTest/Login/GuestView.swift
+++ TravelAgencyTest/Login/GuestView.swift
@@ -8,7 +8,54 @@
 import SwiftUI
 
 struct GuestView: View {
+    @State private var name: String = ""
+    @State private var reservationNumber: String = ""
+    
     var body: some View {
-        Text("Guest View !!")
+        VStack {
+            TextField("예약자 성명", text: $name)
+                .autocapitalization(.none)
+                .disableAutocorrection(true)
+                .textFieldStyle(.plain)
+                .frame(maxWidth: (UIScreen.main.bounds.maxX * 0.8) )
+                .frame(height: (UIScreen.main.bounds.maxY * 0.1) / 2 )
+                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray))
+                .padding(.bottom, 5)
+
+            
+            TextField("예약번호(영문, 숫자 포함 12자리)", text: $reservationNumber)
+                .autocapitalization(.none)
+                .disableAutocorrection(true)
+                .textFieldStyle(.plain)
+                .frame(maxWidth: (UIScreen.main.bounds.maxX * 0.8) )
+                .frame(height: (UIScreen.main.bounds.maxY * 0.1) / 2)
+                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray))
+                .padding(.bottom, 5)
+            
+            HStack {
+                Text("연락처")
+                    .frame(maxWidth: (UIScreen.main.bounds.maxX * 0.15) )
+                    .frame(height: (UIScreen.main.bounds.maxY * 0.1) / 2)
+                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray))
+                    .padding(.bottom, 5)
+                    .foregroundColor(.gray)
+                
+                TextField("ex) 01012345678", text: $reservationNumber)
+                    .autocapitalization(.none)
+                    .disableAutocorrection(true)
+                    .textFieldStyle(.plain)
+                    .frame(maxWidth: (UIScreen.main.bounds.maxX * 0.65) )
+                    .frame(height: (UIScreen.main.bounds.maxY * 0.1) / 2)
+                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray))
+                    .padding(.bottom, 5)
+            } // HStack
+            .frame(maxWidth: (UIScreen.main.bounds.maxX * 0.8))
+        } // VStack
+    }
+}
+
+struct GuestView_Previews: PreviewProvider {
+    static var previews: some View {
+        GuestView()
     }
 }
