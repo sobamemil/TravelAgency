9d95805556302927dd4c0d12290e3ea393b5bf78
sobamemil <sieh96@naver.com>
2021-07-06 11:25:12 +0900

    MemberView의 로그인 버튼의 touch area를 container 크기만큼으로 늘림.

--- TravelAgencyTest/Login/MemberView.swift
+++ TravelAgencyTest/Login/MemberView.swift
@@ -80,6 +80,7 @@ struct MemberView: View {
                 Text("로그인")
                     .font(.system(size: 15))
                     .foregroundColor(.white)
+                    .frame(width: (UIScreen.main.bounds.maxX * 0.8), height: (UIScreen.main.bounds.maxY * 0.1) / 5).padding(.vertical)
                     // .shadow(color: .black, radius: 4, x: 5, y: 5)
             } // Login Button
             .frame(width: (UIScreen.main.bounds.maxX * 0.8), height: (UIScreen.main.bounds.maxY * 0.1) / 5).padding(.vertical)
