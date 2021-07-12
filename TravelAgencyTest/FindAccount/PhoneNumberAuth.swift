25724509e40d71bc1f443017d493d67842825587
sobamemil <sieh96@naver.com>
2021-07-12 16:42:44 +0900

    PhoneNumberAuth.swift 에서 최상위에 ZStack을 추가하고 Alert 구현. VStack 내부를 세 개의 Group으로 나눔. 인증번호 입력창 생성.

--- TravelAgencyTest/FindAccount/PhoneNumberAuth.swift
+++ TravelAgencyTest/FindAccount/PhoneNumberAuth.swift
@@ -13,63 +13,125 @@ struct PhoneNumberAuth: View {
     @State private var rearNumber: String = ""
     
     @State private var selectedTelecom: String = "SKT"
+    @State private var phoneNumber: String = "010"
+    @State private var clickedAuth: Bool = false
+    @State private var authNumber: String = ""
+    
+    @State private var msgWasSent: Bool = false
     
     private let telecomList = ["알뜰통신사", "U+", "KT", "SKT"]
     
+    
     var body: some View {
-        VStack(alignment: .center) {
-            Divider()
-            TextField("홍길동", text: $name)
-                .customTextField(padding: 5, lineWidth: 0)
-            Divider()
-
-            HStack {
-                TextField("xxxxxx", text: $frontNumber)
-                    .multilineTextAlignment(.center)
-                    .keyboardType(.numberPad)
-                
-                Text("-")
-                
-                TextField("xxxxxxx", text: $rearNumber)
-                    .multilineTextAlignment(.center)
-                    .keyboardType(.numberPad)
-            } // HStack
-            Divider()
-                .padding(.bottom)
-            Divider()
-                .padding(.top)
-            
-            Menu {
-                ForEach(0..<telecomList.count) { index in
-                    Button {
-                        selectedTelecom = telecomList[index]
-                    } label: {
-                        if selectedTelecom == telecomList[index] {
-                            HStack {
-                                Text(telecomList[index])
-                                Spacer()
-                                Image(systemName: "checkmark")
-                            }
-                        } else {
-                            Text(telecomList[index])
-                        }
+        ZStack {
+            VStack(alignment: .center) {
+                Group {
+                    Divider()
+                    TextField("홍길동", text: $name)
+                        .customTextField(padding: 5, lineWidth: 0)
+                    Divider()
+                    
+                    HStack {
+                        TextField("xxxxxx", text: $frontNumber)
+                            .multilineTextAlignment(.center)
+                            .keyboardType(.numberPad)
                         
+                        Text("-")
+                        
+                        TextField("xxxxxxx", text: $rearNumber)
+                            .multilineTextAlignment(.center)
+                            .keyboardType(.numberPad)
+                    } // HStack
+                    Divider()
+                        .padding(.bottom)
+                }
+                
+                Group {
+                    Divider()
+                        .padding(.top)
+                    
+                    Menu {
+                        ForEach(0..<telecomList.count) { index in
+                            Button {
+                                selectedTelecom = telecomList[index]
+                            } label: {
+                                if selectedTelecom == telecomList[index] {
+                                    HStack {
+                                        Text(telecomList[index])
+                                        Spacer()
+                                        Image(systemName: "checkmark")
+                                    }
+                                } else {
+                                    Text(telecomList[index])
+                                }
+                            }
+                        }
+                    } label: {
+                        HStack {
+                            Text(selectedTelecom)
+                            Spacer()
+                            Image(systemName: "chevron.down")
+                        } // HStack
+                        .padding(.horizontal, 5)
                     }
-
+                    
+                    Divider()
+                    
+                    HStack {
+                        TextField(phoneNumber, text: $phoneNumber)
+                            .customTextField(padding: 5, lineWidth: 0)
+                        Button {
+                            
+                        } label: {
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(.accentColor)
+                                .overlay(Text("인증하기")
+                                            .foregroundColor(.black))
+                                .frame(maxWidth: 90, maxHeight: 40)
+                            
+                        }
+                    }
+                    Divider()
                 }
-            } label: {
-                HStack {
-                    Text(selectedTelecom)
-                    Spacer()
-                    Image(systemName: "chevron.down")
+                
+                Group {
+                    HStack {
+                        TextField("인증번호 입력", text: $authNumber)
+                            .multilineTextAlignment(.center)
+                            
+                        Button {
+                            
+                        } label: {
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(.accentColor)
+                                .overlay(Text("확인")
+                                            .foregroundColor(.black))
+                                .frame(maxWidth: 90, maxHeight: 40)
+                            
+                        }
+                    }
+                    
+                    Divider()
+                    
+                    HStack {
+                        if msgWasSent {
+                            Text("인증번호가 발송되었습니다.")
+                                .font(.system(size: 12))
+                            
+                        } else {
+                            Text("")
+                        }
+                        Spacer()
+                    }
                 }
+                
+            } // VStack
+            .frame(maxWidth: (UIScreen.main.bounds.maxX * 0.8))
+            
+            if clickedAuth {
+                AlertView(shown: $clickedAuth, isSuccess: clickedAuth, message: "인증번호가 발송되었습니다.", title: "발송 완료", themeColor: Color.accentColor)
             }
-
-            
-            
-            
-        } // VStack
-        .frame(maxWidth: (UIScreen.main.bounds.maxX * 0.8))
+        } // ZStack
     }
 }
 
