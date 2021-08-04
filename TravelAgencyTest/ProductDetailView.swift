efb91372de18c2ee151ac487cadaa15eedff6067
sobamemil <sieh96@naver.com>
2021-08-04 13:18:45 +0900

    ProductDetailView 생성.
    여행 상품 상세페이지 화면. 뷰

---
+++ TravelAgencyTest/ProductDetailView.swift
@@ -0,0 +1,16 @@
+//
+//  ProductDetailView.swift
+//  TravelAgencyTest
+//
+//  Created by 심찬영 on 2021/08/04.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
