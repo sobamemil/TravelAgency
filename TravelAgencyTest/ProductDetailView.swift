f35d1c155a4c9e08af9276818edc934c19124a32
sobamemil <sieh96@naver.com>
2021-08-06 14:45:27 +0900

    detailview 형식 수정.
    대략적인 디자인 방향 제시

--- TravelAgencyTest/ProductDetailView.swift
+++ TravelAgencyTest/ProductDetailView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct ProductDetailView: View {
     @State var date = Date()
+    @State var person = Int()
+    
+    var price: Int
     
     var body: some View {
         VStack {
@@ -20,16 +23,75 @@ struct ProductDetailView: View {
             VStack(alignment: .leading) {
                 HStack {
                     Text("스탠다드")
+                        .foregroundColor(.red)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(.red, lineWidth: 1)
+                        )
+                        
                         
                     Text("출발확정")
+                        .foregroundColor(.orange)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(.orange, lineWidth: 1)
+                        )
                 }
+                .padding(.top)
                 Text("[KE7박8일] 아름다운 절경, 제주 해변 투어, 특가 상품!!")
                     .font(Font.custom("NotoSansKR-Bold", size: 23))
                 
                 DatePicker("출발일", selection: $date, displayedComponents: .date)
+                
+                Text("가격문의")
+            } // VStack
+            .frame(maxWidth: UIScreen.main.bounds.maxX - 40)
+            
+            
+            Button(action: {
+//                    withAnimation {
+//                        loginClicked.toggle()
+//                    }
+            }) {
+                Text("예약하기")
+                    .font(.system(size: 15))
+                    .foregroundColor(.gray)
+                    .frame(width: (UIScreen.main.bounds.maxX * 0.8), height: (UIScreen.main.bounds.maxY * 0.1) / 5).padding(.vertical)
+                    // .shadow(color: .black, radius: 4, x: 5, y: 5)
+            } // Login Button
+            .frame(width: (UIScreen.main.bounds.maxX * 0.8), height: (UIScreen.main.bounds.maxY * 0.1) / 5).padding(.vertical)
+            .background(Color.getThemeColor())
+            .cornerRadius(15)
+            .padding(.vertical)
+            
+            VStack(alignment: .leading) {
+                Text("항공 정보")
+                Divider()
+                    .padding(.bottom)
+                
+                Text("호텔 정보")
+                Divider()
+                    .padding(.bottom)
+                
+                Text("여행 일정")
+                Divider()
+                    .padding(.bottom)
+                
+                Text("옵션")
+                Divider()
+                    .padding(.bottom)
+                
+                Text("여행 약관")
+                Divider()
+                    .padding(.bottom)
             }
             .frame(maxWidth: UIScreen.main.bounds.maxX - 40)
+            .padding(.top)
+            
+            
             Spacer()
+            
+            
             
         }
     }
@@ -38,6 +100,6 @@ struct ProductDetailView: View {
 
 struct ProductDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        ProductDetailView()
+        ProductDetailView(price: 10)
     }
 }
